bec8eef2adb298b35f5a647411d4faa1c8bfdbbb
Gheerwijn Clicque <Gheerwijn@MBPvanGheerwijn.local>
2017-07-23 21:47:51 +0200

    Photography page styling

--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -33,7 +33,7 @@ export const slideInOutAnimation =
             left: 0,
             right: 0,
             bottom: 0,
-            backgroundColor: 'rgba(0, 0, 0, 0.8)'
+        //    backgroundColor: 'rgba(0, 0, 0, 0.8)'
         })),
  
         // route 'enter' transition
@@ -46,23 +46,23 @@ export const slideInOutAnimation =
                 top: '-400%',
  
                 // start with background opacity set to 0 (invisible)
-                backgroundColor: 'rgba(0, 0, 0, 1)'
+  //              backgroundColor: 'rgba(0, 0, 0, 1)'
             }),
  
             // animation and styles at end of transition
-            animate('2s ease-in-out', style({
+            animate('0.5s ease-in-out', style({
                 // transition the right position to 0 which slides the content into view
                 top: '0%',
  
                 // transition the background opacity to 0.8 to fade it in
-                backgroundColor: 'rgba(0, 0, 0, 1)'
+//                backgroundColor: 'rgba(0, 0, 0, 1)'
             }))
         ]),
  
         // route 'leave' transition
         transition('* => void', [
             // animation and styles at end of transition
-            animate('2s ease-in-out', style({
+            animate('0.5s ease-in-out', style({
                 // transition the right position to -400% which slides the content out of view
                 top: '-100%',
  
